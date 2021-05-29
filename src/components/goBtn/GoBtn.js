93990303163fd7be1c80782ebf31a19774e68984
SebaFarias <sebastian.fariasb@usach.cl>
2021-05-29 08:36:05 -0400

    GoBtn refactored

--- src/components/goBtn/GoBtn.js
+++ src/components/goBtn/GoBtn.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import {
+  Button,
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles( theme => ({
+  root:{
+    margin: theme.spacing(4),
+  }
+}))
 
 const GoBtn = () => {
 
+const classes = useStyles()
+
 return (
-  <button title='sumbmitBtn'>
+  <Button 
+    variant="contained" 
+    title='sumbmitBtn' 
+    color='primary'
+    className={classes.root}
+  >
     Ir
-  </button>
+  </Button>
 )
 }
 
